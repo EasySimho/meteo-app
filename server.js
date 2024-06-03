0b6576738b8674d5574d9a1d368e28ca20f37273
EasySimho <s.benanchietti.yt@gmail.com>
2024-06-03 20:11:18 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// Funzione per ottenere l'icona meteo basata sulla condizione
+function weatherIcon(condition) {
+    const iconMap = {
+        'clear-day': 'fas fa-sun',
+        'clear-night': 'fas fa-moon',
+        'rain': 'fas fa-cloud-rain',
+        'snow': 'fas fa-snowflake',
+        'sleet': 'fas fa-cloud-meatball',
+        'wind': 'fas fa-wind',
+        'fog': 'fas fa-smog',
+        'cloudy': 'fas fa-cloud',
+        'partly-cloudy-day': 'fas fa-cloud-sun',
+        'partly-cloudy-night': 'fas fa-cloud-moon',
+        'hail': 'fas fa-cloud-showers-heavy',
+        'thunderstorm': 'fas fa-bolt',
+        'tornado': 'fas fa-poo-storm'
+    };
+
+    return iconMap[condition] || 'fas fa-question';
+}
+
+app.get('/', (req, res) => {
+    res.render('index', { weather: null, error: null });
+});
+
+app.get('/weather', async (req, res) => {
+    const pirateWeatherApiKey = process.env.PIRATE_WEATHER_API_KEY;
+    const openCageApiKey = process.env.OPENCAGE_API_KEY;
+    const city = req.query.city;
+    const geoUrl = `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${openCageApiKey}`;
+
+    try {
+        const geoResponse = await axios.get(geoUrl);
+        const { lat, lng } = geoResponse.data.results[0].geometry;
+        const weatherUrl = `https://api.pirateweather.net/forecast/${pirateWeatherApiKey}/${lat},${lng}?units=si`;
+        const weatherResponse = await axios.get(weatherUrl);
+        const weather = weatherResponse.data;
+
+        // Assegna la classe dell'icona meteo
+        weather.iconClass = weatherIcon(weather.currently.icon);
+
+        // Rendi il template con i dati meteorologici
+        res.render('index', { weather, error: null });
+    } catch (error) {
+        // Se si verifica un errore, rendi il template con un messaggio di errore
+        res.render('index', { weather: null, error: 'City not found' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'selector',
+  content: ["./views/**/*.ejs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
