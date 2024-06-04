fa8a43602a8d3b586396757befa713bb6774e036
EasySimho <s.benanchietti.yt@gmail.com>
2024-06-04 20:36:50 +0200

    port change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5050;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
